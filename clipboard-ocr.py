d380a9798acbd6fd11cda776fa8bb754ce79eb65
Kagami Sascha Rosylight <saschanaz@outlook.com>
2020-12-26 21:20:53 +0100

    feat: support string list from ImageGrab.grabclipboard()

--- clipboard-ocr.py
+++ clipboard-ocr.py
@@ -1,26 +1,36 @@
 import io
 import pyperclip
 from google.cloud import vision
-from google.cloud.vision import types
+from google.cloud.vision_v1 import types
 from PIL import ImageGrab, Image
 
+
+def get_clipboard_image():
+    im = ImageGrab.grabclipboard()
+    if isinstance(im, Image.Image):
+        return im
+    if isinstance(im, list):
+        return Image.open(im[0])
+    raise Exception("No image data in clipboard")
+
+
 # getting image from clipboard
-im = ImageGrab.grabclipboard()
-if isinstance(im, Image.Image):
-    
-    # google vision client
-    client = vision.ImageAnnotatorClient()
-    
-    # loading clipboard to byte array
-    imgByteArr = io.BytesIO()
-    im.save(imgByteArr, "JPEG")
-    
-    # passing image to Goole Vision
-    image = types.Image(content=imgByteArr.getvalue())
-    response = client.text_detection(image=image)
-    
-    # retrieving response
-    texts = response.text_annotations
-    if texts:
-    	# coping regonized text back to clipboard
-        pyperclip.copy(texts[0].description)
+im = get_clipboard_image()
+# google vision client
+client = vision.ImageAnnotatorClient()
+
+# loading clipboard to byte array
+imgByteArr = io.BytesIO()
+im.save(imgByteArr, "PNG")
+
+# passing image to Google Vision
+image = types.Image(content=imgByteArr.getvalue())
+response = client.document_text_detection(image=image)
+
+# retrieving response
+texts = response.text_annotations
+if texts:
+    description = texts[0].description
+    print(description)
+    # coping recognized text back to clipboard
+    pyperclip.copy(description)
